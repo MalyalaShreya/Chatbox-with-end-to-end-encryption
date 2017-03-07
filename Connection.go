235be3061fd5a6fa9c438b258a887e13f15e2798
Nidhi Dhamnani <nidhidhamnani@gmail.com>
2017-03-07 23:20:49 +0530

    Functions added

--- Connection.go
+++ Connection.go
@@ -5,74 +5,35 @@ import "fmt"
 import "bufio"
 import "os"
 import "sync"
-import "crypto/rand"
-import "crypto/rsa"
-import "crypto/sha256"
 import "github.com/trustmaster/go-aspell"
 import "strings"
 import "time"
-import "encoding/gob"
-
 
 type peer struct {
 
-  name string
   IP string
-  port string
   connection net.Conn
-  publicKey *rsa.PublicKey
+  //publicKey *rsa.PublicKey
 
 }
 
-
-type recieve struct {
-
-  name string
-  IP string
-  port string
-  publicKey *rsa.PublicKey
-
-}
 
 type self struct {
 
-  name string
   IP string
-  port string
-  publicKey *rsa.PublicKey
 
 }
 
-type request struct {
-
-  IP string
-  port string
-  conn net.Conn
-
-}
 
 var Self self
-var SelfPrivateKey *rsa.PrivateKey
 
 var AllFriends map[string]peer
-var MyFriends map[string]peer
-var PendingRequests map[string]request
-
-
-func Options() {
-
-  var wg sync.WaitGroup
-  wg.Add(1)
-  //go Listen()
-  go AddFriend()
-  wg.Wait()
-
-}
+var PendingRequests map[string]peer
 
 
 func Listen() {
 
-  ln, _ := net.Listen("tcp", ":"+Self.port)
+  ln, _ := net.Listen("tcp", ":3000")
 
   for {
 
@@ -81,123 +42,116 @@ func Listen() {
       fmt.Println(err.Error())
     }
 
-    ip, port, err := net.SplitHostPort(conn.RemoteAddr().String())
+    ip:=conn.RemoteAddr().String()
+    PendingRequests[ip]=peer{ip,conn}
+    //AllFriends[ip]=peer{ip,conn}
 
-    PendingRequests[ip]=request{ip,port,conn}
+  }
+}
 
-    var response string
-    fmt.Println("You got a friend request!\nDo you want to accept it? (yes/no)")
-    fmt.Scanf("%s",&response)
-    if (response=="yes") {
+func AcceptRequest() {
 
-      handleConn(conn)
+  if len(PendingRequests)!=0 {
+
+    fmt.Print("Enter the IP of the person to accept the request: ")
+    var ip string
+    fmt.Scanf("%s",&ip)
+    AllFriends[ip]=PendingRequests[ip]
+    delete(PendingRequests,ip)
+    fmt.Println(ip," is now your friend!\n")
+
+  } else {
+    fmt.Println("No pending requests!\n")
+  }
+  
+}
+
+func Delete() {
+
+  if len(AllFriends)==0 {
+    fmt.Println("You got no friends!\n")
+  } else {
+
+    fmt.Print("Enter the IP: ")
+    var ip string
+    fmt.Scanf("%s",&ip)
+    if val,ok:=AllFriends[ip];ok {
+      delete(AllFriends,ip)
+      fmt.Println(val.IP,"is no longer your friend now.\n")
 
     } else {
-
-      
-      //delete(sessions, "moo")
-      delete(PendingRequests,ip)
-
-      fmt.Println("Request Declined")
+      fmt.Println(ip, " was not your friend\n")
     }
 
   }
-}
-
-func handleConn(conn net.Conn) {
-
-  ch:=make(chan string)
-
-  go func() {
-    encode(conn)
-  }()
-  go func (){
-    ch<-decode(conn) 
-  }()
-  
-  //var name string
-  <-ch
-  close(ch)
-  fmt.Println("Done")
-
-  
 
 }
 
-//chat(name)
+func chat(ip string) {
+  fmt.Println("-----------------------------------------\n")  
 
-func encode(conn net.Conn) {
+  fmt.Print("To quit the chat with a friend type ':quit'\n")
+  fmt.Println(".........................................\n")
 
-  fmt.Println("Encoder")
-  str := recieve{Self.name,Self.IP,Self.port,Self.publicKey}
-  encoder := gob.NewEncoder(conn)
-  encoder.Encode(str)
-  fmt.Println("Encoder2")
+  if val, ok := AllFriends[ip]; ok { 
 
-}
+    ch1:=make(chan int)
+    ch2:=make(chan int)
 
-func decode(conn net.Conn) (string) {
-  fmt.Println("Decoder1")
-  var person recieve
-  decoder := gob.NewDecoder(conn)
-  decoder.Decode(&person)
-  AllFriends[person.name]=peer{person.name,person.IP,person.port,conn,person.publicKey}
-  MyFriends[person.name]=AllFriends[person.name]
-  fmt.Println("Decoder2")
-  return person.name
+    go func() {
+      ch1<-Write(val.IP)
+    }()
+    go func (){
+      ch2<-Read(val.IP)
+    }()
+    
+    
+    <-ch1
+    <-ch2
+    close(ch1)
+    close(ch2)
+    fmt.Println("---------------------------------------\n")  
+
+  } else {
+
+    fmt.Println(ip, " is not your friend.")
+
+  }
 
 }
 
 
-func chat(name string) {
-
-  ch1:=make(chan int)
-  ch2:=make(chan int)
-
-  go func() {
-    ch1<-Write(name)
-  }()
-  go func (){
-    ch2<-Read(name)
-  }()
-  
-  
-  <-ch1
-  <-ch2
-  close(ch1)
-  close(ch2)
-  fmt.Println("---------------------------------------\n")  
-
-
-}
-
-
-func Read(name string) (int) {
+func Read(ip string) (int) {
 
   for {
-    message, _ := bufio.NewReader(AllFriends[name].connection).ReadString('\n')
-    msg:=decrypt([]byte(message))
-    fmt.Println("Message recieved at: ",time.Now().Format(time.RFC850))
-    fmt.Print("Message Received: ", msg)
-    if(msg==":quit") {
+
+    
+    buff:=make([]byte,0)
+    tmp:=make([]byte,2048)
+    n,_:=AllFriends[ip].connection.Read(tmp)
+    buff=append(buff,tmp[:n]...)
+    //AllFriends[ip].connection.Write([]byte("Seen"))
+    current := time.Now()
+    
+    if(string(buff)==":quit\n") {
+      fmt.Print(ip," went offline now\n")
+      AllFriends[ip].connection.Write([]byte(":quit\n"))
       return 0
     }
+    fmt.Print(ip," (",current.Format("2006-01-02 15:04:05"),"): ", string(buff))
   }
-  
-
 
 }
 
-func Write(name string) (int) {
+func Write(ip string) (int) {
 
   for {
 
     reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
+    //fmt.Print("You: ")
     newmessage, _ := reader.ReadString('\n')
-    if newmessage==":quit" {
-      message:=encrypt(name,newmessage)
-      AllFriends[name].connection.Write([]byte(message + "\n"))
+    if newmessage==":quit\n" {
+      AllFriends[ip].connection.Write([]byte(newmessage))
       return 0
     }
 
@@ -211,111 +165,161 @@ func Write(name string) (int) {
         return 0
     }
 
+
     for _,v := range words {
 
         if speller.Check(v) {
-            //fmt.Print("OK\n")
         } else {
-            fmt.Print(v)
-            fmt.Printf("(suggestions): %s\n", strings.Join(speller.Suggest(v), ", "))
+            fmt.Print("[",v)
+            fmt.Printf("] (Do you mean?): %s\n", strings.Join(speller.Suggest(v), ", "))
         }
 
     }
 
-    message:=encrypt(name,newmessage)
-    fmt.Println("Msg sent at: ",time.Now().Format(time.RFC850))
-    AllFriends[name].connection.Write([]byte(message + "\n"))
+    // fmt.Println("Msg sent at: ",time.Now().Format(time.RFC850))
+    AllFriends[ip].connection.Write([]byte(newmessage))
   }
 
 }
 
-func encrypt(name string,msg string) (string){
 
-  message := []byte(msg)
-  label := []byte("")
-  hash := sha256.New()
-  ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, AllFriends[name].publicKey, message, label)
-
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-
-  fmt.Printf(" encrypted [%s] to \n[%x]\n", string(message), ciphertext)
-  fmt.Println()
-
-  return string(ciphertext)
-}
-
-func decrypt(msg []byte) (string){
-
-
-  label := []byte("")
-  hash := sha256.New()
-  plainText, err := rsa.DecryptOAEP(hash, rand.Reader,SelfPrivateKey,msg,label)
-
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-
-  fmt.Printf(" decrypted [%x] to \n[%s]\n", msg, plainText)
-  return string(plainText)
-
-}
 
 func AddFriend() {
 
-  var friend,friendIP,friendport string
+  var friendIP string
   var mutex = &sync.Mutex{}
   mutex.Lock()
-  fmt.Print("Enter the name of your friend: ")
-  fmt.Scanf("%s",&friend)
   fmt.Print("Enter the IP: ")
   fmt.Scanf("%s",&friendIP)
-  fmt.Print("Enter the port: ")
-  fmt.Scanf("%s",&friendport)
   mutex.Unlock()
 
-  conn, _ := net.Dial("tcp", friendIP+":"+friendport)
-  handleConn(conn)
+  conn, _ := net.Dial("tcp", friendIP)
+
+  AllFriends[friendIP]=peer{friendIP,conn}
+
+}
+
+func showallfriends() {
+
+  if len(AllFriends)==0 {
+    fmt.Println("Forever alone! :(\n")
+  } else {
+    fmt.Println("Friends: ")
+    fmt.Println("---------------------------------------\n")  
+
+    for _,v:=range AllFriends {
+
+      fmt.Println(v.IP)
+
+    }
+    fmt.Println()
+  }
+
+}
+
+func showpendingrequests() {
+
+  if len(PendingRequests)==0 {
+
+    fmt.Println("No requests!\n")
+  } else {
+
+    fmt.Println("Pending requests: ")
+    fmt.Println("---------------------------------------\n")  
+
+    for _,v:=range PendingRequests {
+      fmt.Println(v.IP)
+
+    }
+    fmt.Println()
+  }
+
+  
 }
 
 func main() {
 
   AllFriends = make(map[string]peer)
-  MyFriends = make(map[string]peer)
-  PendingRequests = make(map[string]request)
+  PendingRequests=make(map[string]peer)
+  
+  // fmt.Print("      ---------------------------------------      \nBasic commands\nTo quit the chat with a friend type ':quit'\n      ---------------------------------------      \n")
+  var selfIP string
+  Self = self{IP:""}
 
-  SelfPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 
-  if err != nil {
-    fmt.Println(err.Error)
-    os.Exit(1)
+
+
+
+  
+  go Listen()
+  var i int
+
+  var res bool
+  res=true
+    
+    fmt.Println("\n")
+    fmt.Print("                                                  *********************                                        \n")
+    fmt.Print("                                                  *     MESSENGER     *                             \n")
+    fmt.Print("                                                  *********************                               \n\n")
+    fmt.Print("                                              Username :  ")
+    fmt.Scanf("%s",&selfIP)
+    Self.IP=selfIP
+    fmt.Println("\n")
+
+    fmt.Print("...................................................................................................................\n")
+    fmt.Print(":  MyProfile                                                                      Home  Find Friends  *Active*    :\n")
+    fmt.Print("...................................................................................................................\n\n")
+
+    fmt.Print("--------------  --------------  --------------------  --------------------  ---------  -------------  -------------\n")
+    fmt.Print("|1. AddFriend|  |2. MyFriends|  |3. Friend Requests|  |4. Confirm Request|  |5. Chat|  |6. Unfriend|  |7. Sign out|\n")
+    fmt.Print("--------------  --------------  --------------------  --------------------  ---------  -------------  -------------\n\n")
+
+
+// fmt.Print("--------------","  ","--------------","  ","--------------------","  ","--------------------","  ","---------","  ","-------------","  ","---------\n")
+//     fmt.Print("|1. AddFriend|","  ","|2. MyFriends|","  ","|3. Friend Requests|","  ","|4. Confirm Request|","  ","|5. Chat|","  ","|6. Unfriend|","  ","|7. Quit|\n")
+//     fmt.Print("--------------","  ","--------------","  ","--------------------","  ","--------------------","  ","---------","  ","-------------","  ","---------\n")
+    for res==true {
+      fmt.Print("What's in your mind now?  ")
+
+    // fmt.Println("Select you choice:\n1. AddFriend\n2. See your friends\n3. Show pending friend requests\n4. Accept the friend request\n5. Chat with a friend\n6. Delete a freind\n7. Quit")
+    fmt.Scanf("%d",&i)
+    fmt.Println()
+
+    switch (i) {
+
+    case 1:
+      AddFriend()
+      break
+    case 2:
+      showallfriends()
+      break
+    case 3:
+      showpendingrequests()
+      break
+
+    case 4:
+      AcceptRequest()
+      break
+    case 5: 
+      var ip string
+      fmt.Println("Enter the IP of your friend: ")
+      fmt.Scanf("%s",&ip)
+      chat(ip)
+      break
+    case 6: 
+      Delete()
+      break
+
+    case 7:
+      res=false
+      break
+
+    }
+
   }
 
-  myPublicKey := &SelfPrivateKey.PublicKey
-  fmt.Println("my private key",SelfPrivateKey)
-  fmt.Println("my publickey",myPublicKey)
-  fmt.Print("      ---------------------------------------      \nBasic commands\n1. To quit the chat with a friend type :q\n2. To delete a friend type :delete\n      ---------------------------------------      \n ")
-  var name,self_port,selfIP string
-  Self = self{name:"" ,IP:"",port:"",publicKey:myPublicKey}
-
-
-  fmt.Print("Welcome to your chatbox!\nEnter your name: ")
-  fmt.Scanf("%s",&name)
-  fmt.Print("Enter your public IP: ")
-  fmt.Scanf("%s",&selfIP)
-  fmt.Print("Enter your port: ")
-  fmt.Scanf("%s",&self_port)
   
-  Self.name=name  
-  Self.IP=selfIP
-  Self.port=self_port
-
-  Options()
-  
-  fmt.Print("Bye!\nSee you soon :)");
+  fmt.Print("\nBye!\nSee you soon :)\n");
   
   
 }
